5a8b6e2b61ef996e6ce05d0e1d80bae18323af9a
Young <yangtenghui@dykj81.wecom.work>
2025-04-15 18:04:37 +0800

    ci(ci-test.sh): 注释掉构建和部署相关代码

--- ci-test.sh
+++ ci-test.sh
@@ -12,26 +12,26 @@ set -Exe pipefail
 
 pnpm i
 chmod +x node_modules/.bin/*
-npm run build
+# npm run build
 
 
-
+ls
 # 复制构建产物
 cp -rf .next /www/wwwroot/g-imgs/
 
 
 # 2. 使用进程管理工具 PM2（推荐方案）
-if pm2 list | grep -q 'g-imgs'; then
-    echo "停止现有应用..."
-    pm2 stop g-imgs --wait-ready
-    pm2 delete g-imgs
-fi
+# if pm2 list | grep -q 'g-imgs'; then
+#     echo "停止现有应用..."
+#     pm2 stop g-imgs --wait-ready
+#     pm2 delete g-imgs
+# fi
 
-# 3. 带版本的回滚机制
-sudo mkdir -p "${BACKUP_DIR}"
-[ -d "${DEPLOY_DIR}/.next" ] && sudo cp -rf "${DEPLOY_DIR}/.next" "${BACKUP_DIR}/"
+# # 3. 带版本的回滚机制
+# sudo mkdir -p "${BACKUP_DIR}"
+# [ -d "${DEPLOY_DIR}/.next" ] && sudo cp -rf "${DEPLOY_DIR}/.next" "${BACKUP_DIR}/"
 
-# 4. 使用PM2守护进程
-pm2 start "npm run start" --name g-imgs --time
-pm2 save
-pm2 logs g-imgs --lines 100
+# # 4. 使用PM2守护进程
+# pm2 start "npm run start" --name g-imgs --time
+# pm2 save
+# pm2 logs g-imgs --lines 100
